abea788f223433739089e716d5b3eef2729be1b6
Elias Biondo <contato@eliasbiondo.com>
2022-06-09 16:12:30 -0300

    :sparkles: add pin integration

--- src/Frontend/public/js/pin.js
+++ src/Frontend/public/js/pin.js
@@ -1,5 +1,6 @@
-// Getting user email address element from DOM.
+// Getting all required elements from DOM.
 const userEmailAdressElement = document.querySelector(".user-email-address");
+const submitButton = document.querySelector(".primary-button");
 
 // Instancing the render user email address function.
 function renderUserEmailAddress() {
@@ -12,7 +13,7 @@ function renderUserEmailAddress() {
 // Selecting all pin boxes.
 const pinBoxes = document.querySelectorAll(".pin-box");
 
-// For each pin box, adding a keyup event listener.
+// For each pin box, adding a input event listener.
 pinBoxes.forEach((pinBox) => {
     pinBox.addEventListener("input", function() {
 
@@ -33,5 +34,91 @@ pinBoxes.forEach((pinBox) => {
             // Focusing on the next input pin box.
             nextInput.focus();
         }
+
+        // Checking if the typed pin has the correct character quantity.
+        if(pinBox.value.length > pinBox.getAttribute("maxlength")) {
+            pinBox.value = pinBox.value.slice(0, -1);
+        }
+
       });
 })
+
+// Form prevent default.
+$("form").submit(function(e) {
+    e.preventDefault();
+});
+
+function getAuthenticated() {
+
+    document.querySelector("#loading").style.visibility = "visible";
+
+    typedPin = "";
+    document.querySelectorAll(".pin-box").forEach(pinBox => {
+        typedPin += pinBox.value;
+    })
+
+    var settings = {
+        "url": "http://localhost:4000/api/v1/user/signin",
+        "method": "POST",
+        "timeout": 0,
+        "data": {
+          "email": localStorage.getItem("email-address"),
+          "pin": typedPin
+        }
+      };
+      
+      $.ajax(settings).done(function (response) {
+        if(response.success) {
+            localStorage.setItem("token", `Bearer ${response.success.data.token}`);
+            localStorage.setItem("role", response.success.data.role);
+
+            if(response.success.data.role == 'administrador') {
+                window.location.href = "http://127.0.0.1:5500/public/html/admin/dashboard.html";
+            } else {
+                window.location.href = "http://127.0.0.1:5500/public/html/dashboard.html";
+            }
+        } else {
+            document.querySelector("#loading").style.visibility = "hidden";
+            toastr.error(response.error.detail,response.error.title);
+        }
+      });
+
+}
+
+// Sending the request when user click.
+submitButton.addEventListener("click", function() {
+    getAuthenticated();
+})
+
+const dontReceiveBtn = document.querySelector(".dont-receive");
+
+function sendPin() {
+
+    document.querySelector("#loading").style.visibility = "visible";
+
+    var settings = {
+        "url": "http://localhost:4000/api/v1/user/requestpincode",
+        "method": "POST",
+        "timeout": 0,
+        "data": {
+          "email": localStorage.getItem("email-address"),
+        }
+      };
+      
+      $.ajax(settings).done(function (response) {
+
+        if(response.success) {
+            toastr.success(response.success.title);
+            document.querySelector("#loading").style.visibility = "hidden";
+        } else {
+            document.querySelector("#loading").style.visibility = "hidden";
+            toastr.error(response.error.detail,response.error.title);
+        }
+      });
+
+}
+
+dontReceiveBtn.addEventListener("click", function() {
+    sendPin();
+})
+
