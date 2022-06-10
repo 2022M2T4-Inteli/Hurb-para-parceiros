81e1de1607a7228f3b25734fa9e33ef2df4e6c94
Elias Biondo <contato@eliasbiondo.com>
2022-06-09 23:39:34 -0300

    :sparkles: add get all partners endpoint

--- src/Backend/controllers/api/v1/partner.js
+++ src/Backend/controllers/api/v1/partner.js
@@ -10,10 +10,31 @@ const hasMinimumAdministratorRole = require("../../../middlewares/hasMinimumAdmi
 // Instacing the application router.
 const router = express.Router();
 
+router.get("/", auth, hasMinimumPartnerRole, async (req, res) => {
+  Database.open(__dirname + '../../../../database/database.db').then(async (db) => {
+    // Getting all users from database.
+    let users = await db.all(`SELECT * FROM Parceiro`);
+
+    // Returning the success message response.
+    res.send({
+      "status": 200,
+      "success": {
+        "code": 0,
+        "title": "Users gotted successfully",
+        "data": users,
+        "source": {
+          "pointer": "/controllers/api/v1/user.js"
+        }
+      }
+    });
+
+  })
+})
+
 router.post("/create", auth, hasMinimumAdministratorRole, async (req, res) => {
 
     // Getting all required attributes from request body.
-    const { accountable_id, full_name, telephone, cpf } = req.body.partner;
+    const { accountable_id, full_name, telephone, cpf } = req.body;
 
     // Executing the action...
     Database.open(__dirname + '../../../../database/database.db').then(async (db) => {
@@ -29,8 +50,8 @@ router.post("/create", auth, hasMinimumAdministratorRole, async (req, res) => {
               "status": 401,
               "error": {
                 "code": 0,
-                "title": "Invalid accountable id.",
-                "detail": "The accountable id provided are not registered in our database. It is not possible to create a partner without provide a valid accountable id.",
+                "title": "Invalid accountable id or cpf already registered",
+                "detail": "The accountable id provided are not registered in our database or the inputed cpf is already registered in our database.",
                 "source": {
                   "pointer": "/controllers/api/v1/order.js"
                 }
@@ -39,7 +60,7 @@ router.post("/create", auth, hasMinimumAdministratorRole, async (req, res) => {
           )
         }
         
-        partner.info = await db.get(`SELECT * FROM Parceiro WHERE "id_do_usuario_responsavel" = ${accountable_id}`);
+        partner.info = await db.get(`SELECT * FROM Parceiro WHERE "id_do_usuario_responsavel" = ${accountable_id} AND "cpf"="${cpf}"`);
 
         res.send({
             "status": 200,
