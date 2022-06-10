688c2391799a64247683f1b2188f442efb13de52
Elias Biondo <contato@eliasbiondo.com>
2022-06-10 00:03:42 -0300

    :sparkles: add create booking integration

--- src/Frontend/public/js/admin/create-booking.js
+++ src/Frontend/public/js/admin/create-booking.js
@@ -47,3 +47,89 @@ elements.inputs.forEach(input => {
     });
 })
 
+// Setting up the page minimum access level.
+const pageMinimumAccessLevel = 10;
+
+// Setting up the delay function.
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
+}
+
+// Setting up the notification and redirect function.
+const invalidOrExpiredToken = async () => {
+
+    document.querySelector("#loading").style.visibility = "hidden";
+
+    toastr.error("Faça o login novamente. Redirecionando...","Token inválido ou sessão expirada");
+
+    await delay(3);
+
+    window.location.href = "http://127.0.0.1:5500/public/html/login.html";
+
+}
+
+// Checking if the user's session still valid.
+$(document).ready(function() {
+
+    if(!localStorage.getItem("token")) {
+        invalidOrExpiredToken();
+    } else {
+
+        document.querySelector("#loading").style.visibility = "visible";
+
+        var settings = {
+            "url": "http://localhost:4000/api/v1/user/is-session-token-still-valid",
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+              "Authorization": localStorage.getItem("token"),
+            },
+            "error": invalidOrExpiredToken,
+          };
+          
+          $.ajax(settings).done(function (response) {
+            if(response.success) {
+
+                document.querySelector("#loading").style.visibility = "hidden";
+
+                if(response.success.data.access_level < 10) {
+                    invalidOrExpiredToken();
+                }
+                
+            }
+          })
+
+    }
+
+});
+
+let organizations = [];
+
+$(document).ready(function() {
+    var settings = {
+        "url": "http://localhost:4000/api/v1/organization/",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+          "Authorization": localStorage.getItem("token"),
+        },
+      };
+      
+      $.ajax(settings).done(function (response) {
+        response.success.data.forEach(organization => {
+
+            console.log(organization);
+
+            organizations.push(organization);
+
+            let option = document.createElement("option");
+            option.textContent = `${organization.nome} - ${organization.cnpj}`;
+            option.id = organization.id;
+
+            document.querySelector("#responsible-organization").appendChild(option);
+
+        })
+      });
+})
