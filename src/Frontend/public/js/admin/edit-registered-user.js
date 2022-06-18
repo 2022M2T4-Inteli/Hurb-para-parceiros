67172bc548dc9a30dcbeaae63cc9437ad4a2694d
Elias Biondo <contato@eliasbiondo.com>
2022-06-18 20:57:23 -0300

    :sparkles: add edit and delete user integration

--- src/Frontend/public/js/admin/edit-registered-user.js
+++ src/Frontend/public/js/admin/edit-registered-user.js
@@ -1,9 +1,42 @@
 $(document).ready(function() {
+    var settings = {
+        "url": "http://localhost:4000/api/v1/user/",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+          "Authorization": localStorage.getItem("token"),
+        },
+    };
 
-	const submitBtn = document.querySelector(".primary-button");
+    document.querySelector("#loading").style.visibility = "visible";
+      
+    $.ajax(settings).done(function (response) {
 
-	submitBtn.addEventListener("click", function() {
-        window.location.href = "http://127.0.0.1:5500/public/html/admin/edit-user.html";
-    })
+        if (response.success) {
 
+            const users = response.success.data;
+
+            users.forEach(user => {
+                let option = document.createElement("option");
+                option.textContent = user.email;
+                option.value = user.email;
+                option.id = user.id;
+                document.querySelector("#edit-registered-user").appendChild(option);
+            })
+
+        } else {
+
+            toastr.error(response.error.detail, response.error.title); 
+
+        }
+
+        document.querySelector("#loading").style.visibility = "hidden";
+        
+        
+    });
+})
+
+document.querySelector(".primary-button").addEventListener("click", function() {
+    sessionStorage.setItem("target-edit-id", document.querySelector(`option[value='${(document.querySelector("#edit-registered-user").value)}']`).id);
+    document.location.href="http://127.0.0.1:5500/public/html/admin/edit-user.html";
 })
--- src/Frontend/public/js/admin/edit-user.js
+++ src/Frontend/public/js/admin/edit-user.js
@@ -40,3 +40,93 @@ elements.inputs.forEach(input => {
 
     });
 })
+
+
+$(document).ready(function() {
+
+    var settings = {
+        "url": `http://localhost:4000/api/v1/user/u/${sessionStorage.getItem("target-edit-id")}`,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+          "Authorization": localStorage.getItem("token"),
+        },
+    };
+      
+    $.ajax(settings).done(function (response) {
+
+         user = response.success.data;
+
+         document.querySelector("#email").value = user.email;
+
+         var settings = {
+            "url": "http://localhost:4000/api/v1/role/",
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+              "Authorization": localStorage.getItem("token")
+            },
+          };
+          
+          $.ajax(settings).done(async function (response) {
+            await response.success.data.forEach(role => {
+    
+                let option = document.createElement("option");
+                option.textContent = role.nome;
+                option.value = role.nome;
+                option.id = role.id;
+    
+                document.querySelector("#role").appendChild(option);
+    
+            })
+
+            document.querySelector("#role").value = document.querySelector(`option[id="${user.id_do_cargo}"]`).value;
+
+          });
+
+          isFilled.firstInput = true;
+
+          buttonStatusHandler();
+
+          document.querySelector(".primary-button").addEventListener("click", function() {
+            var settings = {
+                "url": `http://localhost:4000/api/v1/user/u/${user.id}`,
+                "method": "PUT",
+                "timeout": 0,
+                "headers": {
+                  "Authorization": localStorage.getItem("token"),
+                },
+                "data": {
+                    email:document.querySelector("#email").value,
+                    role: document.querySelector("#role").value
+                }
+            };
+              
+            $.ajax(settings).done(function (response) {
+                window.location.href = "http://127.0.0.1:5500/public/html/admin/edit-registered-user.html";
+            });
+          })
+
+          document.querySelector("#delete-btn").addEventListener("click", function() {
+            
+            var settings = {
+                "url": `http://localhost:4000/api/v1/user/u/${user.id}`,
+                "method": "DELETE",
+                "timeout": 0,
+                "headers": {
+                  "Authorization": localStorage.getItem("token"),
+                },
+              };
+              
+            $.ajax(settings).done(function (response) {
+                window.location.href = "http://127.0.0.1:5500/public/html/admin/edit-registered-user.html";
+            });
+
+          })
+          
+
+    });
+
+})
+
+
