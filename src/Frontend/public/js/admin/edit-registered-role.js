dc119afb76af0f29492b04bffb3794b0c6af5dd0
andlljr <andre.junior@sou.inteli.edu.br>
2022-06-18 16:01:39 -0300

    :wrench: Fix link role pages

---
+++ src/Frontend/public/js/admin/edit-registered-role.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	const submitBtn = document.querySelector(".primary-button");
+
+	submitBtn.addEventListener("click", function() {
+        window.location.href = "http://127.0.0.1:5500/public/html/admin/edit-role.html";
+    })
+
+})
